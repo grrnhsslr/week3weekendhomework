0b12bb2b18ce85ebf27403071e644f8d87441999
grrnhsslr <hasslergarren@gmail.com>
2024-03-11 22:41:26 -0500

    added visual break characters to space out the output a bit, making it easier to read. added delete function

--- tasksToDo.py
+++ tasksToDo.py
@@ -19,17 +19,37 @@ def run_tasklist():
 
     # start 'running' the to do list until the user quits
     while True:
-        task_choice = input('1 View incomplete tasks:\n2. To view a single task by ID\n3. To create a new task\n4. Mark a task complete\n5. View completed tasks\nor Type or type "quit" to quit \n')
+        task_choice = input('1 View incomplete tasks:\n2. To view a single task by ID\n3. To create a new task\n4. Mark a task complete\n5. View completed tasks\n6. to delete a task\nor Type or type "quit" to quit \n')
         if task_choice == '1':
+            print('=' * 50)
             todo.view_tasks()
+            print('=' * 50)
+            time.sleep(1)
         elif task_choice == '2':
+            print('=' * 50)
             todo.view_task()
+            print('=' * 50)
+            time.sleep(2)
         elif task_choice == '3':
+            print('=' * 50)
             todo.add_task()
+            print('=' * 50)
+            time.sleep(2)
         elif task_choice == '4':
+            print('=' * 50)
             todo.finished_tasks()
+            print('=' * 50)
+            time.sleep(2)
         elif task_choice == '5':
+            print('=' * 50)
             todo.view_completed_tasks()
+            print('=' * 50)
+            time.sleep(2)
+        elif task_choice == '6':
+            print('=' * 50)
+            todo.delete_task()
+            print('=' * 50)
+            time.sleep(2)
         elif task_choice == 'quit':
             break
 
